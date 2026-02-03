abc1cd93ed311202c7d040d19bacc02a15ddfce4
OscarThroedsson <oscar.throedsson@gmail.com>
2026-02-03 14:35:28 +0100

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import PlayerSection from "./components/PlayerSection";
+import Scoreboard from "./components/Scoreboard";
+import { GameProvider } from "./provider/GameProvider";
+import StartGame from "./components/StartGame";
 import "./App.css";
 
 function App() {
   return (
-    <main>
-      <button>Start Game</button>
-    </main>
+    <GameProvider>
+      <main className="h-screen w-screen grid grid-rows-[auto_auto] lg:grid-cols-2 lg:grid-rows-1 ">
+        <Scoreboard />
+        <StartGame />
+        <PlayerSection index={0} />
+        <PlayerSection index={1} />
+      </main>
+    </GameProvider>
   );
 }
 
