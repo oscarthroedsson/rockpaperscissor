ceb6d21c7202e1130c54db5ea8fd123a08a8fe5e
OscarThroedsson <oscar.throedsson@gmail.com>
2026-02-04 15:13:33 +0100

    removed app.css and added provider logic n styling to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import PlayerSection from "./components/PlayerSection";
 import Scoreboard from "./components/Scoreboard";
 import { GameProvider } from "./provider/GameProvider";
 import StartGame from "./components/StartGame";
-import "./App.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
 
 function App() {
   return (
-    <GameProvider>
-      <main className="h-screen w-screen grid grid-rows-[auto_auto] lg:grid-cols-2 lg:grid-rows-1 ">
-        <Scoreboard />
-        <StartGame />
-        <PlayerSection index={0} />
-        <PlayerSection index={1} />
-      </main>
-    </GameProvider>
+    <QueryClientProvider client={queryClient}>
+      <GameProvider>
+        <main className="h-screen w-screen grid grid-rows-[auto_auto] lg:grid-cols-2 lg:grid-rows-1 ">
+          <Scoreboard />
+          <StartGame />
+          <PlayerSection player="player1" />
+          <div
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+             text-5xl font-bold text-white"
+            style={{
+              fontFamily: '"Fredoka One", cursive',
+              textShadow: `
+      -2px -2px 0 #000,
+       2px -2px 0 #000,
+      -2px  2px 0 #000,
+       2px  2px 0 #000
+    `,
+            }}
+          >
+            VS
+          </div>
+          <PlayerSection player="player2" />
+        </main>
+      </GameProvider>
+    </QueryClientProvider>
   );
 }
 
