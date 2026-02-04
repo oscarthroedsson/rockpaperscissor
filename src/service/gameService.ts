b03ba15a930d2934e4609c954d2a1b25a04d8731
OscarThroedsson <oscar.throedsson@gmail.com>
2026-02-04 15:15:21 +0100

    changed service to better handle errors

--- src/service/gameService.ts
+++ src/service/gameService.ts
@@ -1,46 +1,72 @@
 import type { MoveEnum } from "../game.types";
 
 const BASE_URL = "https://rps-api-ujymak7y3a-ew.a.run.app";
-
+// const BASE_URL = "/api";
 export class GameService {
-  getAll() {
-    return fetch(`${BASE_URL}/games`, {
+  async getAll() {
+    const res = await fetch(`${BASE_URL}/games`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
     });
+
+    if (!res.ok) {
+      const errTxt = await res.text();
+      console.error("Error | [GameService]-[getAll] | txt: ", res.status, errTxt);
+      throw new Error(`Error | [GameService]-[getAll]`);
+    }
+
+    return res.json();
   }
 
-  getGame(gameId: string) {
-    return fetch(`${BASE_URL}/games/${gameId}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  async getGame(gameId: string) {
+    const res = await fetch(`${BASE_URL}/games/${gameId}`);
+
+    if (!res.ok) {
+      const errTxt = await res.text();
+      console.error("Error | [GameService]-[getGame] | txt: ", res.status, errTxt);
+      throw new Error(`Error | [GameService]-[getAll]`);
+    }
+    return res.json();
   }
 
-  getRound(gameId: string, roundId: string) {
-    return fetch(`${BASE_URL}/games/${gameId}/round/${roundId}`, {
+  async getRound(gameId: string, roundId: string) {
+    const res = await fetch(`${BASE_URL}/games/${gameId}/round/${roundId}`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
     });
+
+    if (!res.ok) {
+      const errTxt = await res.text();
+      console.error("Error | [GameService]-[getRound] | txt: ", res.status, errTxt);
+      throw new Error(`Error | [GameService]-[getRound]`);
+    }
+
+    return res.json();
   }
 
   async createGame() {
-    return fetch(`${BASE_URL}/games`, {
+    const res = await fetch(`${BASE_URL}/games`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
     });
+
+    if (!res.ok) {
+      const errTxt = await res.text();
+      console.error("Error | [GameService]-[createGame] | txt: ", res.status, errTxt);
+      throw new Error(`Error | [GameService]-[createGame]`);
+    }
+
+    return res.json();
   }
 
   async addPlayer(name: string, gameId: string) {
-    return fetch(`${BASE_URL}/games/${gameId}/addPlayer`, {
+    const res = await fetch(`${BASE_URL}/games/${gameId}/addPlayer`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -49,10 +75,18 @@ export class GameService {
         name,
       }),
     });
+
+    if (!res.ok) {
+      const errTxt = await res.text();
+      console.error("Error | [GameService]-[addPlayer] | txt: ", res.status, errTxt);
+      throw new Error(`Error | [GameService]-[addPlayer]`);
+    }
+
+    return res.json();
   }
 
-  move(playerId: string, gameId: string, move: MoveEnum) {
-    return fetch(`${BASE_URL}/games/${gameId}/doMove`, {
+  async move(playerId: string, gameId: string, move: MoveEnum) {
+    const res = await fetch(`${BASE_URL}/games/${gameId}/doMove`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -63,5 +97,15 @@ export class GameService {
         move,
       }),
     });
+
+    if (!res.ok) {
+      const errTxt = await res.text();
+      console.error("Error | [GameService]-[move] | txt: ", res.status, errTxt);
+      throw new Error(`Error | [GameService]-[move]`);
+    }
+
+    return res.json();
   }
 }
+
+export const gameServiceSingelton = new GameService();
