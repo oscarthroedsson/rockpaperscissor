8012e9e6d08f4cd13d1fa1d76b7132eec33bc79a
OscarThroedsson <oscar.throedsson@gmail.com>
2026-02-05 12:36:13 +0100

    applyed end-game mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import PlayerSection from "./components/PlayerSection";
 import Scoreboard from "./components/Scoreboard";
 import { GameProvider } from "./provider/GameProvider";
 import StartGame from "./components/StartGame";
+import EndGame from "./components/EndGame";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,6 +21,7 @@ function App() {
         <main className="h-screen w-screen grid grid-rows-[auto_auto] lg:grid-cols-2 lg:grid-rows-1 ">
           <Scoreboard />
           <StartGame />
+          <EndGame />
           <PlayerSection player="player1" />
           <div
             className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
